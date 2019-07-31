6cef278af9f48f429c87e6eb7f43c6aff9cc161f
poojaguleria <guleriapooja93@gmail.com>
2019-07-31 09:40:42 +0530

    implement pagination.

---
+++ app/src/main/java/com/example/projectassignment/addresslist/ItemDataSourceFactory.java
@@ -0,0 +1,24 @@
+package com.example.projectassignment.addresslist;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.paging.DataSource;
+import androidx.paging.PageKeyedDataSource;
+
+import com.example.projectassignment.addresslist.AddressDataSource;
+import com.example.projectassignment.data.models.DeliveryAddress;
+
+public class ItemDataSourceFactory extends DataSource.Factory {
+
+    public MutableLiveData<PageKeyedDataSource<Integer, DeliveryAddress>> addressLiveDataSource = new MutableLiveData<>();
+
+    @Override
+    public DataSource create() {
+        AddressDataSource addressDataSource = new AddressDataSource();
+        addressLiveDataSource.postValue(addressDataSource);
+        return addressDataSource;
+    }
+
+    public MutableLiveData<PageKeyedDataSource<Integer, DeliveryAddress>> getAddressLiveDataSource() {
+        return addressLiveDataSource;
+    }
+}
